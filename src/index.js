bd1bb082a79ee1698d0da1648c927476236a85ab
chikuwa111 <take0shun1111@gmail.com>
2018-07-05 02:34:11 +0900

    Make state persistent

--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ import * as React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 import reducer from './reducers'
 import registerServiceWorker from './lib/registerServiceWorker'
 import App from './components/App'
 
 const root = document.getElementById('root')
 if (root instanceof Element) {
-  const store = createStore(reducer)
+  const persistConfig = {
+    key: 'root',
+    storage,
+  }
+  const persistedReducer = persistReducer(persistConfig, reducer)
+  const store = createStore(persistedReducer)
+  const persistor = persistStore(store)
   render(
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>,
     root
   )
