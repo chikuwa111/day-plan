de27f6aa0277b245b65f345cb92027bd6c63257c
chikuwa111 <take0shun1111@gmail.com>
2018-07-04 22:43:02 +0900

    Delete Form

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,5 @@
 // @flow
 import type { TasksAction } from './tasks'
-import type { FormAction } from './form'
 import type { StockAction } from './stock'
 
-export type Action = TasksAction | FormAction | StockAction
+export type Action = TasksAction | StockAction
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,11 @@ import type { State } from '../types'
 import type { Action } from '../actions'
 import tasks from './tasks'
 import setting from './setting'
-import form from './form'
 import stock from './stock'
 
 const reducer: (state: State, action: Action) => State = combineReducers({
   tasks,
   setting,
-  form,
   stock,
 })
 
