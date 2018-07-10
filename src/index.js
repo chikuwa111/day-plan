9a6d6f9bb87bbb0d0773a33d7807a171eb8ca1b5
chikuwa111 <take0shun1111@gmail.com>
2018-07-10 12:31:46 +0900

    Refactor store

--- src/index.js
+++ src/index.js
@@ -1,29 +1,16 @@
 // @flow
 import * as React from 'react'
 import { render } from 'react-dom'
-import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import { persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
-import logger from 'redux-logger'
-import reducer from './reducers'
+import store from './lib/store'
 import registerServiceWorker from './lib/registerServiceWorker'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import App from './components/App'
 
 const root = document.getElementById('root')
 if (root instanceof Element) {
-  const persistConfig = {
-    key: 'root',
-    storage,
-    blacklist: ['session'],
-  }
-  const persistedReducer = persistReducer(persistConfig, reducer)
-  const store =
-    process.env.NODE_ENV === 'production'
-      ? createStore(persistedReducer)
-      : createStore(persistedReducer, applyMiddleware(logger))
   const persistor = persistStore(store)
 
   render(
---
+++ src/lib/store.js
@@ -0,0 +1,22 @@
+// @flow
+import { applyMiddleware, createStore } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import reducers from '../reducers'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['session'],
+}
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+let middleware = []
+if (process.env.NODE_ENV === 'production') {
+  const logger = require('redux-logger')
+  middleware = [...middleware, logger]
+}
+
+const store = createStore(persistedReducer, applyMiddleware(...middleware))
+
+export default store
