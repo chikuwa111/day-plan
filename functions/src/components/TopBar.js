e978707dd6c056530c82c9d08a6f6278a430aeb7
chikuwa111 <take0shun1111@gmail.com>
2018-08-27 02:08:05 +0900

    Fix title for not found

--- functions/src/components/TopBar.js
+++ functions/src/components/TopBar.js
@@ -15,7 +15,7 @@ type Props = {|
 
 export default function TopBar(props: Props) {
   const { plan } = props
-  const title = plan != null ? plan.title : 'NOT FOUNDあああ'
+  const title = plan != null ? plan.title : 'NOT FOUND'
   const encodedTitle = encodeURI(title)
   const shareUrl =
     plan == null || plan.cloudId == null
