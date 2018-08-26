5cea57cd8f0a804018e11d9c11a25aec2845d412
chikuwa111 <take0shun1111@gmail.com>
2018-08-26 22:57:49 +0900

    Implement ssr style

--- functions/src/components/App.js
+++ functions/src/components/App.js
@@ -3,8 +3,6 @@ import * as React from 'react'
 import pure from 'recompose/pure'
 import styled from 'styled-components'
 import type { Plan } from '../../../src/types'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-import blueGrey from '@material-ui/core/colors/blueGrey'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import TopBar from '../components/TopBar'
 import TimeTable from '../components/TimeTable'
@@ -18,21 +16,10 @@ export default pure(function App(props: Props) {
   const { plan } = props
 
   return (
-    <MuiThemeProvider theme={theme}>
+    <div>
       <CssBaseline />
       <TopBar title={plan != null ? plan.title : '404 NOTFOUND'} />
       {plan == null ? <NotFound /> : <TimeTable plan={plan} />}
-    </MuiThemeProvider>
+    </div>
   )
-})
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: blueGrey[300],
-      main: blueGrey[600],
-      dark: blueGrey[900],
-      contrastText: '#fff',
-    },
-  },
 })
--- functions/src/index.js
+++ functions/src/index.js
@@ -3,6 +3,15 @@ import functions from 'firebase-functions'
 import admin from 'firebase-admin'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
+import { ServerStyleSheet } from 'styled-components'
+import { SheetsRegistry } from 'react-jss/lib/jss'
+import JssProvider from 'react-jss/lib/JssProvider'
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  createGenerateClassName,
+} from '@material-ui/core/styles'
+import blueGrey from '@material-ui/core/colors/blueGrey'
 import express from 'express'
 import fs from 'fs'
 import App from './components/App'
@@ -21,20 +30,60 @@ app.get('/p/:id', (req, res) => {
     .doc(id)
     .then(documentSnapshot => {
       const plan = documentSnapshot.data()
-
       const status = plan != null ? 200 : 404
       const title = plan != null ? plan.title : '404 NOTFOUND'
-      const html = renderToString(<App plan={plan} />)
 
-      const finalHtml = index
-        .replace('::OG_TITLE::', id)
-        .replace('::PLAN_ID::', id)
-        .replace('<!-- ::TITLE:: -->', id)
-        .replace('<!-- ::APP:: -->', html)
+      const html = renderPage(id, title, plan)
 
       res.set('Cache-Control', 'public, max-age=600, s-maxage=1200')
-      res.status(status).send(finalHtml)
+      res.status(status).send(html)
     })
 })
+
+function renderPage(id, title, plan) {
+  // for styled-components
+  const sheet = new ServerStyleSheet()
+
+  // for material-ui
+  const sheetsRegistry = new SheetsRegistry()
+  const sheetsManager = new Map()
+  const generateClassName = createGenerateClassName()
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        light: blueGrey[300],
+        main: blueGrey[600],
+        dark: blueGrey[900],
+        contrastText: '#fff',
+      },
+    },
+  })
+
+  const html = renderToString(
+    sheet.collectStyles(
+      <JssProvider
+        registry={sheetsRegistry}
+        generateClassName={generateClassName}
+      >
+        <MuiThemeProvider theme={theme} sheetsManager={sheetsManager}>
+          <App plan={plan} />
+        </MuiThemeProvider>
+      </JssProvider>
+    )
+  )
+
+  const styledComponentsStyle = sheet.getStyleTags()
+  const materialUiStyle = sheetsRegistry.toString()
+
+  const finalHtml = index
+    .replace('::OG_TITLE::', id)
+    .replace('::PLAN_ID::', id)
+    .replace('<!-- ::TITLE:: -->', title)
+    .replace('<!-- ::APP:: -->', html)
+    .replace('<!-- ::STYLED_COMPONENTS_STYLE:: -->', styledComponentsStyle)
+    .replace('/* ::MATERIAL_UI_STYLE */', materialUiStyle)
+  return finalHtml
+}
 
 export const ssrapp = functions.https.onRequest(app)
